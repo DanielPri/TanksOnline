28ef570b0e5cb91bf7b5e9984ca56d1265b46a30
DanielPri <privorotskyd@gmail.com>
2020-03-21 14:31:44 -0400

    setting up a PUN lobby

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField] public string sceneToLoad = "Tank_Game";
+    [SerializeField] private GameObject findOpponentPanel = null;
+    [SerializeField] private GameObject waitingStatusPanel = null;
+    [SerializeField] private TextMeshProUGUI waitingStatusText = null;
+
+    private bool isConnecting = false;
+
+    private const string GameVersion = "0.1";
+    private const int maxPlayersPerRoom = 2;
+
+    private void Awake() => PhotonNetwork.AutomaticallySyncScene = true;
+
+    public void FindOpponent()
+    {
+        isConnecting = true;
+        findOpponentPanel.SetActive(false);
+        waitingStatusPanel.SetActive(true);
+
+        waitingStatusText.text = "Searching...";
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        } else
+        {
+            PhotonNetwork.GameVersion = GameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        waitingStatusPanel.SetActive(false);
+        findOpponentPanel.SetActive(true);
+
+        Debug.Log($"Disconnected due to: {cause}");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"No clients are waiting for an opponent, creating a new room");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Client successfully joined room");
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        if(playerCount != maxPlayersPerRoom)
+        {
+            waitingStatusText.text = "Waiting for Opponent";
+            Debug.Log("Client is waiting for an opponent");
+        }
+        else
+        {
+            waitingStatusText.text = "Opponent Found";
+            Debug.Log("Match is ready to begin");
+        }
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if(PhotonNetwork.CurrentRoom.PlayerCount == maxPlayersPerRoom)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            Debug.Log("Match is ready to begin");
+            waitingStatusText.text = "Opponent Found";
+
+            PhotonNetwork.LoadLevel(sceneToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/PlayerNameInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField] TMP_InputField nameInputField = null;
+    [SerializeField] Button continueButton = null;
+
+    private const string PlayerPrefsNameKey = "PlayerName";
+
+    private void Start() => SetUpInputField();
+
+    private void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+        nameInputField.text = defaultName;
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName()
+    {
+        string playerName = nameInputField.text;
+        PhotonNetwork.NickName = playerName;
+
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
+    }
+}
