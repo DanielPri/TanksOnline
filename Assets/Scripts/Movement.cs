a1c5820e88987045e0a501c24f1a720f42eea7df
DanielPri <privorotskyd@gmail.com>
2020-03-22 12:50:30 -0400

    added multiplayer movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviourPun
+{
+    [SerializeField] private float movementSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            TakeInput();
+        }
+    }
+
+    private void TakeInput()
+    {
+        Vector3 movement = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        }.normalized;
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject PlayerPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate(PlayerPrefab.name, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
