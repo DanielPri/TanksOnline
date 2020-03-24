e0e09cc4c24f690577de452d70e4b8a1effaae02
DanielPri <privorotskyd@gmail.com>
2020-03-24 13:52:27 -0400

    added rotation and shell fire

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class Cannon : MonoBehaviourPun
+{
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private GameObject shellPrefab;
+    [SerializeField] private Transform spawnPoint;
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            TakeInput();
+        }
+    }
+
+    private void TakeInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            photonView.RPC("FireProjectile", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    private void FireProjectile()
+    {
+        var shellInstance = Instantiate(shellPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        shellInstance.GetComponent<Rigidbody>().velocity = shellInstance.transform.forward * projectileSpeed;
+        Destroy(shellInstance, 5f);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Movement : MonoBehaviourPun
 {
     [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
 
     // Update is called once per frame
     void Update()
@@ -21,11 +22,19 @@ public class Movement : MonoBehaviourPun
     {
         Vector3 movement = new Vector3
         {
-            x = Input.GetAxisRaw("Horizontal"),
+            x = 0f, // Input.GetAxisRaw("Horizontal"),
             y = 0f,
             z = Input.GetAxisRaw("Vertical")
         }.normalized;
 
+        Vector3 rotation = new Vector3
+        {
+            x = 0f,
+            y = Input.GetAxisRaw("Rotation"),
+            z = 0f
+        };
+
         transform.Translate(movement * movementSpeed * Time.deltaTime);
+        transform.Rotate(rotation * rotationSpeed * Time.deltaTime);
     }
 }
