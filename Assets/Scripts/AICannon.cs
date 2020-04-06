7ec4709dbefdea58e5bbaac5f8a57e22b7592dc6
DanielPri <privorotskyd@gmail.com>
2020-04-06 19:02:43 -0400

    Tank AI decisions are only performed by master

--- Assets/Scripts/AICannon.cs
+++ Assets/Scripts/AICannon.cs
@@ -3,22 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AICannon : MonoBehaviour
+public class AICannon : MonoBehaviourPun
 {
 
     [SerializeField] private GameObject shellPrefab;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private float cooldown = 0.5f;
+    [SerializeField]
+    private float range = 10f;
     private float timeElapsedSinceFire = 0;
     private void Update()
     {
+        if (photonView.IsMine)
+        {
+            HandleCannon();
+        }
+    }
+
+    private void HandleCannon()
+    {
         RaycastHit hit;
         Ray ray = new Ray(spawnPoint.position, spawnPoint.forward);
-        if(Physics.Raycast(ray,out hit))
+        if (Physics.Raycast(ray, out hit))
         {
             if (hit.transform.tag == "Player")
             {
-                Fire();   
+                if ((hit.transform.position - transform.position).magnitude < range)
+                {
+                    Fire();
+                }
             }
         }
         timeElapsedSinceFire += Time.deltaTime;
